5058043122a9e72855a4f0b09d9fae642dbdd707
USER <USER@LAPTOP-NBA1G7PD>
2021-10-19 15:23:08 +0530

    Commit 2

--- CRM_Maven_Project/src/test/java/com/crm/contactTest/ContactTest.java
+++ CRM_Maven_Project/src/test/java/com/crm/contactTest/ContactTest.java
@@ -16,11 +16,7 @@ public class ContactTest {
 		String username = System.getProperty("user");
 		 System.out.println("======="+username+"========");
 	}
-	@Test
-	public void deleteContactTest()
-	{
-		System.out.println("execute deleteContactTest");
-	}
+	
 	
 
 }
